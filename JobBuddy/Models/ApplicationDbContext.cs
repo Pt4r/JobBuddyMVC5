b29a06ed035de11a25396c90c4a12f6a3841e464
Pt4r <palaiologosstr@hotmail.com>
2019-12-20 18:37:28 +0200

    Connection to Pasparakis DB

---
+++ JobBuddy/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace JobBuddy.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("PasparakisDB", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+}
--- JobBuddy/Models/ApplicationUser.cs
+++ JobBuddy/Models/ApplicationUser.cs
@@ -18,16 +18,4 @@ namespace JobBuddy.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }

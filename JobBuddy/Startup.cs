19bddb94f0912a577b266dbedb69c00b1ef80e76
Pt4r <palaiologosstr@hotmail.com>
2019-12-20 18:06:38 +0200

    Add project files.

---
+++ JobBuddy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobBuddy.Startup))]
+namespace JobBuddy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

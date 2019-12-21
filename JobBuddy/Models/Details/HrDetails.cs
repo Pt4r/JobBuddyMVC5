dfc33a5316d66749050057f3a219789df79a0f11
Spyros Misichronis <Spyros.Misichronis@Peoplecert.onmicrosoft.com>
2019-12-21 20:32:06 +0200

    HrDetails, JobListing Models

---
+++ JobBuddy/Models/Details/HrDetails.cs
@@ -0,0 +1,18 @@
+﻿using JobBuddy.Models.Job;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobBuddy.Models
+{
+    public class HrDetails
+    {
+        public int Id { get; set; }
+        public Company Company {get;set;}
+        public List<JobListing> JobListings { get; set; }
+        public int PhoneNumber { get; set; }
+        public string ProfilePic { get; set; }
+
+    }
+}
---
+++ JobBuddy/Models/Job/JobListing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobBuddy.Models.Job
+{
+    public class JobListing
+    {
+        //+ HR :type 
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime PostDate { get; }
+        public string Info { get; set; }
+        public JobCategory JobCategory { get; set; }
+        public List<Client> SubmitedClients { get; set; }
+        public Company Company { get; set; }
+
+        public JobListing()
+        {
+            PostDate = DateTime.Now;
+        }
+
+    }
+}

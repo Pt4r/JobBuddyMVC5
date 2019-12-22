2b89e67f4f6916095c851211f3b72d7fad7ddc69
Stratos Paleo <palaiologosstr@hotmail.com>
2019-12-22 18:48:56 +0200

    Controllers and basic CRUD without file handling

--- JobBuddy/Models/ClientUserDetails.cs
+++ JobBuddy/Models/ClientUserDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,10 @@ namespace JobBuddy.Models
 {
     public class ClientUserDetails
     {
-        public Guid id { get; set; }
+        public Guid Id { get; set; }
         public string ProfilePicture { get; set; }
         public int PhoneNumber { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
         public Gender Gender { get; set; }
         public CurrentStatus CurrentStatus { get; set; }

dcd1181af13f64cb0ced12e75679800333250e56
Spyros Misichronis <Spyros.Misichronis@Peoplecert.onmicrosoft.com>
2019-12-23 20:59:53 +0200

    more fluent

--- JobBuddy/Models/ApplicationUser.cs
+++ JobBuddy/Models/ApplicationUser.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;

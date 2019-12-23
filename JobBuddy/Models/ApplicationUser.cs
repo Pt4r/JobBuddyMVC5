01675c2d86a7e51cedf63bcdcdc57b3582eeb2d0
Spyros Misichronis <Spyros.Misichronis@Peoplecert.onmicrosoft.com>
2019-12-23 20:59:38 +0200

    added some fluent.api in ApplicationDbContext

--- JobBuddy/Models/ApplicationUser.cs
+++ JobBuddy/Models/ApplicationUser.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;

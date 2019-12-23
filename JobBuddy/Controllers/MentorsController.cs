f2ff2b1e2d8afdfda267525658a387c0c9de60bb
spyrostheofilatos <spyros.theofilatos@gmail.com>
2019-12-24 01:25:08 +0200

    controllers

---
+++ JobBuddy/Controllers/MentorsController.cs
@@ -0,0 +1,24 @@
+﻿using JobBuddy.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JobBuddy.Models;
+
+namespace JobBuddy.Controllers
+{
+    public class MentorsController : Controller
+    {
+        // GET: Mentors
+        private MentorRepository _mentorRepository = new MentorRepository();
+        
+        public ActionResult Index()
+        {
+            var mentors = _mentorRepository.GetMentors();
+            return View(mentors);
+        }
+
+
+    }
+}

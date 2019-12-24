3d44e1e3caec0514d53ec0d7e4bd7c04c39b196a
delossoled <pmpakogeorgos@gmail.com>
2019-12-24 21:46:23 +0200

    Delos1

---
+++ JobBuddy/Models/JobCategories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobBuddy.Models
+{
+    public enum enumJobCategoryTitle { IT , Economics, Marketing, Sales, Engineering, Healthcare, Arts, Tourism, CustomerSupport } 
+    public class JobCategories
+    { 
+        public enumJobCategoryTitle JobCategoryTitle { get; }
+        public string Subcategory111 { get; set; }
+
+        public string Subcategory222 { get; set; }
+    }
+}

58b4defdd769288fe77a33711261ed33ef14712d
Spyros Misichronis <Spyros.Misichronis@Peoplecert.onmicrosoft.com>
2019-12-24 20:48:23 +0200

    minnor fixes

--- JobBuddy/Models/ApplicationDbContext.cs
+++ JobBuddy/Models/ApplicationDbContext.cs
@@ -26,7 +26,7 @@ namespace JobBuddy.Models
         {
             modelBuilder.Entity<HrDetail>().ToTable("HrUser").HasKey(i => i.Id);
             modelBuilder.Entity<HrDetail>().Property(i => i.PhoneNumber).IsRequired();
-            modelBuilder.Entity<HrDetail>().HasManyHasMany(i => i.HrCompany).WithMany(i => i.HrUsers).Map(m => m.ToTable("HrCompany"));
+            modelBuilder.Entity<HrDetail>().HasMany(i => i.HrCompany).WithMany(i => i.HrUsers).Map(m => m.ToTable("HrCompany"));
             modelBuilder.Entity<HrDetail>().HasMany(i => i.JobListings).WithRequired(c => c.HrUser).HasForeignKey(c => c.HrUserId);
 
             base.OnModelCreating(modelBuilder);
